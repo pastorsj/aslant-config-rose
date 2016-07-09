6c5acae433106bb3c90eb4df701bfee5b5698ac3
Sam Pastoriza <pastorsj@rose-hulman.edu>
2016-07-08 20:46:31 -0700

    Initial commit of linter

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# aslant-config-rose
+# eslint-config-rose
 The Rose-Hulman ESLint configuration file
+
+## Installing the eslint config
+```
+npm install eslint eslint-config-rose --save-dev
+```
+## Usage
+In a .eslintrc file
+```
+{
+  "extends": "rose"
+}
+```
+In the package.json file
+```
+  "devDependencies": {
+		"eslint": "^2.7.0",
+		"eslint-config-rose": "^1.0.0"
+	},
+	"eslintConfig": {
+		"extends": "rose"
+	}
+```
+
---
+++ index.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2016 Rose-Hulman Institute of Technology. All rights reserved.
+ */
+'use strict'
+
+module.exports = {
+  extends: 'xo',
+  globals: {
+    goog: true
+  },
+  rules: {
+    'indent': [2, 2, {
+      SwitchCase: 1
+    }],
+    'space-before-function-paren': [2, 'never'],
+    'valid-jsdoc': [2, {
+      requireReturn: true,
+      prefer: {
+        returns: 'return'
+      }
+    }],
+    'require-jsdoc': 1,
+    'max-len': [1, 120, 4, {
+      ignoreComments: true,
+      ignoreUrls: true
+    }],
+    'new-cap': ['error', {
+      'capIsNew': false
+    }],
+
+    //  Resetting things that eslint-config-xo has an opinion about, but the
+    //  Google Style Guide doesn't.
+    'curly': 0,
+    'no-floating-decimal': 0,
+    'no-unused-vars': [2, {
+      'args': 'none'
+    }]
+  }
+}
